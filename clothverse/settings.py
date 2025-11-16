5579758612b8698cbe0b7d6d41210bb0afee408a
Kartik-devp <kartiktelote0804@gmail.com>
2025-11-16 19:28:24 +0530

    Change database to SQLite for Render deployment

--- clothverse/settings.py
+++ clothverse/settings.py
@@ -84,15 +84,11 @@ WSGI_APPLICATION = 'clothverse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES ={
-	'default':{
-	     'ENGINE':'django.db.backends.mysql',
-	     'NAME': 'clothverse_db',
-	      'USER': 'root',
-	      'PASSWORD': '123456',
-	      'HOST': 'localhost',
-	      'PORT':3306,
-	}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 import cloudinary
